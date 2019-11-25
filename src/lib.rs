fccc0334222e08d276efcfeb48f6bbef8f4a71be
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-11-25 19:06:19 +0300

    Add Future based implementation

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,15 @@ mod ws;
 
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
+use futures::channel::{mpsc, oneshot};
 use js_sys::{Function, Promise};
 use mute_unmute_poc_proto::{Command, Event};
+use std::pin::Pin;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::{spawn_local, JsFuture};
 
 use crate::ws::WebSocket;
+use std::future::Future;
 
 #[derive(Eq, PartialEq, Hash)]
 struct PeerId(pub i32);
@@ -45,22 +48,14 @@ impl PromiseResolver {
 struct Room {
     peers: HashMap<PeerId, PeerConnection>,
     ws: WebSocket,
-    promise_repo: PromiseRepository,
 }
 
 impl Room {
     pub fn handle_event(&mut self, event: Event) {
-        self.promise_repo
-            .take_all_resolvers_for_event(&event)
-            .into_iter()
-            .for_each(|resolver| {
-                resolver.resolve();
-            });
-
         match event {
             Event::RoomMuted { video, audio } => {
                 self.peers.iter_mut().for_each(|(id, peer)| {
-                    peer.mute(video, audio);
+                    peer.mute(audio, video);
                 });
             }
         }
@@ -98,11 +93,9 @@ impl RoomHandle {
     pub fn new() -> Self {
         console_error_panic_hook::set_once();
         let ws = WebSocket::new("ws://127.0.0.1:10000/ws");
-        let room = Rc::new(RefCell::new(Room {
-            promise_repo: PromiseRepository::new(),
-            peers: HashMap::new(),
-            ws,
-        }));
+        let mut peers = HashMap::new();
+        peers.insert(PeerId(100), PeerConnection::new());
+        let room = Rc::new(RefCell::new(Room { peers, ws }));
         let room_clone = room.clone();
         room.borrow_mut().ws.on_message(move |event| {
             room_clone.borrow_mut().handle_event(event);
@@ -112,39 +105,21 @@ impl RoomHandle {
 
     pub fn mute(&self, audio: bool, video: bool) -> Promise {
         let (resolver, promise) = PromiseResolver::new_promise();
-        self.0
+        let qq: Vec<_> = self
+            .0
             .borrow_mut()
-            .promise_repo
-            .insert_resolver(Event::RoomMuted { audio, video }, resolver);
+            .peers
+            .iter_mut()
+            .map(|(_, peer)| peer.on_mute(audio, video))
+            .collect();
+
+        spawn_local(async move {
+            futures::future::join_all(qq).await;
+            resolver.resolve();
+        });
+
         self.0.borrow().ws.send(Command::MuteRoom { audio, video });
-
         promise
-    }
-}
-
-struct PromiseRepository(Rc<RefCell<HashMap<Event, Vec<PromiseResolver>>>>);
-
-impl PromiseRepository {
-    pub fn new() -> Self {
-        Self(Rc::new(RefCell::new(HashMap::new())))
-    }
-
-    pub fn insert_resolver(&self, event: Event, resolver: PromiseResolver) {
-        self.0
-            .borrow_mut()
-            .entry(event)
-            .or_insert_with(|| Vec::new())
-            .push(resolver);
-    }
-
-    pub fn take_all_resolvers_for_event(
-        &self,
-        event: &Event,
-    ) -> Vec<PromiseResolver> {
-        self.0
-            .borrow_mut()
-            .remove(event)
-            .unwrap_or_else(|| Vec::new())
     }
 }
 
@@ -153,32 +128,73 @@ struct PeerConnection {
 }
 
 impl PeerConnection {
+    pub fn new() -> Self {
+        Self {
+            tracks: vec![
+                Sender::new(SenderKind::Video),
+                Sender::new(SenderKind::Audio),
+            ],
+        }
+    }
+
     pub fn mute(&mut self, audio: bool, video: bool) {
-        if audio {
+        self.tracks
+            .iter_mut()
+            .filter(|sender| {
+                (&sender.kind == &SenderKind::Audio && audio)
+                    || (&sender.kind == &SenderKind::Video && video)
+            })
+            .for_each(|sender| sender.mute());
+    }
+
+    pub fn on_mute(
+        &mut self,
+        audio: bool,
+        video: bool,
+    ) -> impl Future<Output = Vec<Result<(), oneshot::Canceled>>> {
+        futures::future::join_all(
             self.tracks
-                .iter()
-                .filter(|sender| &sender.kind == &SenderKind::Audio)
-                .for_each(|sender| sender.mute());
-        }
-        if video {
-            self.tracks
-                .iter()
-                .filter(|sender| &sender.kind == &SenderKind::Video)
-                .for_each(|sender| sender.mute())
-        }
+                .iter_mut()
+                .filter(|sender| {
+                    (&sender.kind == &SenderKind::Audio && audio)
+                        || (&sender.kind == &SenderKind::Video && video)
+                })
+                .map(|sender| sender.on_mute()),
+        )
     }
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 enum SenderKind {
     Video,
     Audio,
 }
 
+#[derive(Debug)]
 struct Sender {
     kind: SenderKind,
+    on_mute: Vec<oneshot::Sender<()>>,
 }
 
 impl Sender {
-    pub fn mute(&self) {}
+    pub fn new(kind: SenderKind) -> Self {
+        Self {
+            kind,
+            on_mute: Vec::new(),
+        }
+    }
+
+    pub fn mute(&mut self) {
+        self.on_mute
+            .drain(..)
+            .for_each(|on_mute| on_mute.send(()).unwrap());
+    }
+
+    pub fn on_mute(
+        &mut self,
+    ) -> impl Future<Output = Result<(), oneshot::Canceled>> {
+        let (tx, rx) = oneshot::channel();
+        self.on_mute.push(tx);
+        rx
+    }
 }
